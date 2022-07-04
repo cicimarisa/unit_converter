1a369e0852db4652e853eccfb4b97dbf9137b605
Cici Marisa Hardianti <30491674+cicimarisa@users.noreply.github.com>
2022-07-04 21:52:42 +0800

    Fixing alignment
    Fixing alignment

--- index.js
+++ index.js
@@ -20,23 +20,26 @@ convert.addEventListener("click", function(){
 		error.style.display = "none"
 	} else{
 		error.style.display = "inline"
+		meterFeet.innerHTML = ""
+		literGallon.innerHTML = ""
+		kilogramsPounds.innerHTML = ""
 	}
 })
 
 function convertMeterFeet(unitToConvert){
 	const meterToFeet = (unitToConvert * 3.281).toFixed(3)
 	const feetToMeter = (unitToConvert / 3.281).toFixed(3)
-	return meterFeet.textContent = `${unitToConvert} meters = ${meterToFeet} feet | ${unitToConvert} feet = ${feetToMeter} meters`
+	return meterFeet.innerHTML = `<p>${unitToConvert} meters = ${meterToFeet} feet | ${unitToConvert} feet = ${feetToMeter} meters</p>`
 }
 
 function convertLiterGallon(unitToConvert){
 	const literToGallon = (unitToConvert * 0.264).toFixed(3)
 	const gallonToLiter = (unitToConvert / 0.264).toFixed(3)
-	return literGallon.textContent = `${unitToConvert} meters = ${literToGallon} feet | ${unitToConvert} feet = ${gallonToLiter} meters`
+	return literGallon.innerHTML = `<p>${unitToConvert} meters = ${literToGallon} feet | ${unitToConvert} feet = ${gallonToLiter} meters</p>`
 }
 
 function convertKilogramsPounds(unitToConvert){
 	const kilogramsToPounds = (unitToConvert * 2.204).toFixed(3)
 	const poundsToKilograms = (unitToConvert / 2.204).toFixed(3)
-	return kilogramsPounds.textContent = `${unitToConvert} meters = ${kilogramsToPounds} feet | ${unitToConvert} feet = ${poundsToKilograms} meters`
+	return kilogramsPounds.innerHTML = `<p>${unitToConvert} meters = ${kilogramsToPounds} feet | ${unitToConvert} feet = ${poundsToKilograms} meters</p>`
 }
