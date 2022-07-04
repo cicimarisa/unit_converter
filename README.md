39772907182a200121d9e5a0b3e79d7dfcbd74c4
Cici Marisa Hardianti <30491674+cicimarisa@users.noreply.github.com>
2022-07-04 20:44:32 +0800

    Initial Commit
    Initial Commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Unit Conversion
+
+## Scrimba Challange 3rd Module, Javascript Part 3 - Cici Marisa Hardianti
+
+This code is built from the scratch 🚀
+
+- [Challenge's link](https://scrimba.com/learn/frontend/solo-project-unit-converter-cz9aPNSr)
+- [Designs's link](https://scrimba.com/learn/frontend/solo-project-unit-converter-cz9aPNSr)
---
+++ index.js
@@ -0,0 +1,42 @@
+/*
+1 meter = 3.281 feet
+1 liter = 0.264 gallon
+1 kilogram = 2.204 pound
+*/
+
+const convert = document.getElementById("convert")
+const unit = document.getElementById("unit")
+const meterFeet = document.getElementById("meter-feet")
+const literGallon = document.getElementById("liter-gallon")
+const kilogramsPounds = document.getElementById("kilograms-pounds")
+const error = document.getElementById("error")
+
+
+convert.addEventListener("click", function(){
+	if(unit.value){
+		convertMeterFeet(unit.value)
+		convertLiterGallon(unit.value)
+		convertKilogramsPounds(unit.value)
+		error.style.display = "none"
+	} else{
+		error.style.display = "inline"
+	}
+})
+
+function convertMeterFeet(unitToConvert){
+	let meterToFeet = (unitToConvert * 3.281).toFixed(3)
+	let feetToMeter = (unitToConvert / 3.281).toFixed(3)
+	return meterFeet.textContent = `${unitToConvert} meters = ${meterToFeet} feet | ${unitToConvert} feet = ${feetToMeter} meters`
+}
+
+function convertLiterGallon(unitToConvert){
+	let literToGallon = (unitToConvert * 0.264).toFixed(3)
+	let gallonToLiter = (unitToConvert / 0.264).toFixed(3)
+	return literGallon.textContent = `${unitToConvert} meters = ${literToGallon} feet | ${unitToConvert} feet = ${gallonToLiter} meters`
+}
+
+function convertKilogramsPounds(unitToConvert){
+	let kilogramsToPounds = (unitToConvert * 2.204).toFixed(3)
+	let poundsToKilograms = (unitToConvert / 2.204).toFixed(3)
+	return kilogramsPounds.textContent = `${unitToConvert} meters = ${kilogramsToPounds} feet | ${unitToConvert} feet = ${poundsToKilograms} meters`
+}
